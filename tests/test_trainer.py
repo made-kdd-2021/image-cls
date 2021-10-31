0f9cab39e51fb5dff19165470d237152014be1b9
KernelA <17554646+KernelA@users.noreply.github.com>
2021-10-31 13:04:17 +0300

    Load Grayscale image

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -1,5 +1,4 @@
 import pytest
-from unittest import mock
 from hydra import compose, initialize, utils
 
 from training import PneumoniaClsTrainer
@@ -12,10 +11,13 @@ def test_trainer(is_scheduler,  image_batch_info, model):
     with initialize(config_path=CONFIG_PATH):
         cfg = compose(config_name="training")
         loss = utils.instantiate(cfg.loss)
+
+        scheduler = cfg.get("scheduler", None)
+
         model = PneumoniaClsTrainer(model=model, optimizer_config=cfg.optimizer,
                                     loss_module=loss,
                                     class_labels=["1", "2"],
-                                    scheduler_config=cfg.scheduler if is_scheduler else None)
+                                    scheduler_config=scheduler)
 
         model.on_train_start()
         model.on_train_epoch_start()
--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -6,24 +6,23 @@ from transforms import ResizeWithPadding
 
 from .conftest import CONFIG_PATH
 
+
 @pytest.mark.parametrize("max_size", [100, 300, 400])
 @pytest.mark.parametrize("aspect_ratio", [0.5, 1.5])
-def test_transform(max_size, aspect_ratio, image_batch):
+def test_transform(max_size, aspect_ratio, rgb_image_batch):
     tr = ResizeWithPadding(max_size=max_size, aspect_ratio=aspect_ratio)
 
-    for image in image_batch:
+    for image in rgb_image_batch:
         resized = tr(image)
         assert resized.shape[-1] == resized.shape[-2] == max_size
 
+
 @pytest.mark.parametrize("transform_type", ["train_transform", "test_transform"])
-def test_config_transform(transform_type, image_batch):
+def test_config_transform(transform_type, rgb_image_batch):
     with initialize(config_path=CONFIG_PATH):
         cfg = compose(config_name="training")
-        transform = torch.jit.script(utils.instantiate(getattr(cfg.transforms, transform_type)))
-        transformed = transform(image_batch)
+        transform = utils.instantiate(getattr(cfg.transforms, transform_type))
+        transformed = transform(rgb_image_batch)
         assert transformed.shape[-1] == transformed.shape[-2]
-        assert transformed.shape[-3] == 3
+        assert transformed.shape[-3] == rgb_image_batch.shape[-3]
         assert transformed.dtype == torch.float
-
-
-
